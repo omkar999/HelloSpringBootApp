2455ab63d3e514cb671212ba4656639de79d359e
omkar <omkar.p89@gmail.com>
2015-12-26 15:44:24 +0530

    Added angulartest() controller method inorder to support my first angularJs $htt.get() call in AngularJS Repo

--- src/main/java/com/nextgen/app/controller/HelloController.java
+++ src/main/java/com/nextgen/app/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.nextgen.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,30 @@ public class HelloController {
 	public String greet() {
 		return "Greetings from Spring Boot! " + user.getName()
 				+ ". This is Bean Config Test using @Configuration for custom bean definitions";
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/angulartest/getuser", headers = "Accept=application/json")
+	public List<User> angulartest() {
+
+		List<User> users = new ArrayList<User>();
+		User user = new User();
+		user.setName("omkar");
+		user.setCity("Harrisburgh");
+
+		User user1 = new User();
+		user1.setName("John");
+		user1.setCity("New York");
+
+		User user2 = new User();
+		user2.setName("Ben");
+		user2.setCity("California");
+
+		users.add(user);
+		users.add(user1);
+		users.add(user2);
+
+		return users;
 	}
 
 }
--- src/main/java/com/nextgen/app/entities/User.java
+++ src/main/java/com/nextgen/app/entities/User.java
@@ -1,8 +1,20 @@
 package com.nextgen.app.entities;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName(value = "user")
 public class User {
 
 	String name;
+	String city;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 
 	public String getName() {
 		return name;
